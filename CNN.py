5353103d5f350261cf2241ae423ba1c6175845e2
burnsg5 <burnsg5@mcmaster.ca>
2025-03-28 18:42:18 -0400

    Commented code

--- CNN.py
+++ CNN.py
@@ -1,34 +1,35 @@
 import tensorflow as tf
-
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv("data/typedCSV.csv")
-data = data.sample(frac=1)
-labels=data['label']
-data = data.iloc[:,1:]
+data = pd.read_csv("data/typedCSV.csv") # load data
+data = data.sample(frac=1) # randomize order
+labels=data['label'] # labels
+data = data.iloc[:,1:] # images
 
-training_length = int(len(labels)*0.6)
+training_length = int(len(labels)*0.6) # proportion for training
 x_train = np.array(data.iloc[:training_length])
 y_train = np.array(labels.iloc[:training_length])
 x_test = np.array(data.iloc[training_length:])
 y_test = np.array(labels.iloc[training_length:])
 
+# reshape
 x_train = x_train.reshape(len(y_train), 28, 28)
 x_test = x_test.reshape(len(y_test), 28, 28)
+
+# normalize
 x_train=x_train / 255.0
 x_test=x_test/255.0
 
-
-
+# model hyper parameters
 input_shape = (28, 28, 1)
 num_classes = 62
-
 batch_size = 64
 epochs = 5
 
+# convert labels correct ouptut type
 y_train = tf.one_hot(y_train.astype(np.int32), depth=num_classes)
 y_test = tf.one_hot(y_test.astype(np.int32), depth=num_classes)
 
@@ -49,6 +50,7 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08), loss='categorical_crossentropy', metrics=['acc'])
 
+# Early trainig stop
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if(logs.get('acc')>0.995):
@@ -65,17 +67,6 @@ history = model.fit(x_train, y_train,
 
 
 model.save("models/charCNN.keras")
-
-'''
-fig, ax = plt.subplots(2,1)
-ax[0].plot(history.history['loss'], color='b', label="Training Loss")
-ax[0].plot(history.history['val_loss'], color='r', label="Validation Loss",axes =ax[0])
-legend = ax[0].legend(loc='best', shadow=True)
-
-ax[1].plot(history.history['acc'], color='b', label="Training Accuracy")
-ax[1].plot(history.history['val_acc'], color='r',label="Validation Accuracy")
-legend = ax[1].legend(loc='best', shadow=True)'
-'''
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f"test accuracy: {test_acc}")
--- text_recognition.py
+++ text_recognition.py
@@ -8,16 +8,15 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.lines import Line2D
 from matplotlib.collections import PatchCollection
-from pytesseract import pytesseract 
 import tensorflow as tf
-#import easyocr
 
-image_path = 'images/PandP_C1/P9.png'
-model = tf.keras.models.load_model('models/charCNN.keras')
-#reader = easyocr.Reader(['en'], gpu=False, )
+
+image_path = 'images/PandP_C1/P1.png' #path to image
+model = tf.keras.models.load_model('models/charCNN.keras') #path to model
 
 def grayscale(a):
   '''
+  Convert RGB image to grey
   a - rbg image as numpy array
   returns greyscale image
   '''
@@ -31,6 +30,7 @@ def grayscale(a):
 
 def binary(a, T=200):# takes np array image
   '''
+  Convert greyscale to binary image
   a - numpy array of greyscale image
   T = threshold
   return binary image based on threshold
@@ -47,6 +47,7 @@ def binary(a, T=200):# takes np array image
 
 def line_coords(coords):
     '''
+    Isolate bounding coordinates of line segments
     coords - cooridinate of edges in line
     returns line boundry rectangle
     '''
@@ -65,6 +66,7 @@ def line_coords(coords):
 
 def char_coords(coords, line):
     '''
+    Isolate bounding cooridnates of characters within a line semgment
     coords - cooridinate of edges in character
     returns character boundry rectangle
     '''
@@ -83,6 +85,7 @@ def char_coords(coords, line):
 
 def line_segment(a):
   '''
+  Find all in segments
   a - nparray of binary image
   returns - boundaries of each line
 
@@ -132,74 +135,80 @@ def line_segment(a):
 def char_segment(line, image):
   
   '''
+  Find all characters in a line segment
+
   line - boundary of line
   image - nparray of binary image
   returns - boundaries of each character
   '''
 
-  # funntion needs to have better transition between letter - some letter are close enough to get grouped together
+  # funtion needs to have better transition between letter - some letter are close enough to get grouped together
   
-  coords=[] #coordinates with text, on edge change
+  coords=[] #coordinates with text, boundaries
   xy_rect = [] # line detection info
-  spaces = []
-  space_size = []
-  a = image[line[0]:line[1],line[2]:line[3]+3]# added 3 so that last character was detected - todo find better fix
+  spaces = [] # for tracking location of spaces
+  space_size = [] # for tracking lengths of gaps between letters
+  a = image[line[0]:line[1],line[2]:line[3]+3]# added 3 so that last character was able to be detected 
   
   space_count = 0
-  spaces = []
-  first_row = -1
-  last_row = -1
-  first_row_prev = -1
-  last_row_prev = -1
-  char_width = 0
-  char_width_sum = 0
-  prev_char_width = 0
-  for i in range(len(a[0,:])): #for each col in image
-    coo=[]
-    flag=0
 
-    count = 0 # track col number
-    col_gap = 0
-    col_gap_max = 0
-    prev_gap_max = 0
+  first_row = -1 # coordinate of fist non-empty row in column
+  last_row = -1 # cooridate of last empy row in column
+  first_row_prev = -1 # coordinate of first non-empy row in previos column
+  last_row_prev = -1 # coordinate of late non-empy row in previous column
+  
+  char_width = 0 # track width of current character
+  char_width_sum = 0 # to be used for average charaxcter width
+  prev_char_width = 0 # width of previous character
+
+  for i in range(len(a[0,:])): #for each col in image
+    coo=[] # track locations of col pixels
+    flag=0 # flag if column has text
+
+    count = 0 # track row number
+    col_gap = 0 # track length of empty space in column
+    col_gap_max = 0 
     
     for c in a[:,i]: # iterate through all rows in column
 
-      if c<200: # check col has any text in it
+      if c<200: # check row has text
+       
+        # update and reset cloumn gap tracker
         if col_gap > col_gap_max:
            col_gap_max = col_gap
         col_gap = 0
+       
+        # update first row
         if first_row == -1:
           first_row = count
           last_row = count
+        
+        # update last row
         else:
           last_row = count
 
-
-
-        #if space_count > prev_char_width*0.3:
+        #add space, rest counter
         spaces.append(i+line[2]-space_count/2)
         space_size.append(space_count)
-        
-
         space_count = 0
+        
         flag=1
       
+      # empty row in column
       else:
           col_gap += 1
 
-      count +=1
-    '''
-    if (abs(first_row - last_row) < 3) and (abs(first_row_prev - last_row_prev) > abs(first_row - last_row)): # avoid connecting artefacts
-      flag = 0
-    '''
+      # 
+      count +=1 # next row
 
-    letter_thresh=0.2
+    letter_thresh=0.2 # propertion of full line wideth needed to classify as character
+
     if flag==1: # col had text somewhere
+      
       char_width += 1
+      
       # check for letters very close together
-      #(first_row != -1 and first_row_prev != -1) and abs(last_row_prev - first_row) > letter_thresh*len(a[:,i]) and abs(first_row_prev - last_row) > letter_thresh*len(a[:,i]) and abs(last_row_prev - first_row) > letter_thresh*len(a[:,i]) and abs(first_row_prev - last_row) > letter_thresh*len(a[:,i]) and abs(first_row_prev - first_row) > letter_thresh*len(a[:,i]) and abs(last_row_prev - last_row) > letter_thresh*len(a[:,i]) and''' 
-      if ((first_row > last_row_prev and first_row > first_row_prev) or (last_row < last_row_prev and last_row < first_row_prev)): # still needs to spearate My and ay
+      if ((first_row > last_row_prev and first_row > first_row_prev) or (last_row < last_row_prev and last_row < first_row_prev)): # still needs to seperate My and ay
  
           if len(coords)> 0: # end line
               xy_rect.append(char_coords(coords, line))
@@ -220,67 +229,51 @@ def char_segment(line, image):
           coo.append([last_row, i]) # add to list of edge changed
       except:
         pass
-      
-      '''
-      for b in range(len(a[:,i])): # look for edge changes
-        
-        # no
-        if a[b][i]>200: # if no text in current row
-          try:
-            if a[b+1][i] <200:  # if text in the row to the below
-              coo.append([b+1, i]) # add to list of edge changed
-          except:
-            pass
-
-        if a[b][i]<200: #if text in current row
-          try:
-            if a[b+1][i] > 200: # if next row empty
-              coo.append([b,i]) # add to list
-              if b == 0:
-                 print("0 row added")
-    
-          except:
-            pass
-        '''
         
   
     else: # col had no text
       space_count +=1
       
-
-      if len(coords)> 0: # end line0
-          char_rect = char_coords(coords, line)
-          if abs(char_rect[0]-char_rect[1]) > letter_thresh*(line[0]-line[1]):
-            xy_rect.append(char_coords(coords, line))
+      if len(coords)> 0: # end line
+          xy_rect.append(char_coords(coords, line))
           coords=[]
+
+          # for calculating average char width
           if char_width > prev_char_width:
-            prev_char_width = char_width # avoid thin letters
+            prev_char_width = char_width 
           char_width_sum += char_width
           char_width = 0
 
-    
+    # update for next column
     first_row_prev = first_row
     last_row_prev = last_row
     first_row = -1
     last_row = -1
-    prev_gap_max = col_gap_max
     col_gap_max = 0
 
-    if len(coo)>0:
+    if len(coo)>0: # add column coords to character cords
       coords.append(coo)
-  if(len(xy_rect) != 0):
-    av_char = char_width_sum/len(xy_rect)
-  else:
+
+  # average width
+  try: av_char = char_width_sum/len(xy_rect)
+  except:
      av_char = 0
+  
+  # decide if spaces are between letters in owrd or between words
   true_spaces = []
   for i in range(len(space_size)):
      if space_size[i] > 0.6*av_char:
-        #space_size = np.delete(space_size, i)
         true_spaces.append(spaces[i])
 
   return xy_rect, true_spaces
 
 def upsample_image(image, factor):
+    '''
+    Convert char image from to new larger size
+    image - original image
+    factor - what to scale image by
+    returns - new image of desired size
+    '''
     mn = np.shape(image)
     m = mn[0]
     n = mn[1]
@@ -299,6 +292,12 @@ def upsample_image(image, factor):
     return new_image
 
 def downsample_image(image, factor):
+    '''
+    Convert char image to new smaller size
+    image - original image
+    factor - what to scale image down by
+    returns - new image of desired size
+    '''
     mn = np.shape(image)
     m = mn[0]
     n = mn[1]
@@ -317,21 +316,31 @@ def downsample_image(image, factor):
     return new_image
 
 def char_seg_2_28(image, char):
+    '''
+    converts character order info to 28 by 28 pixel image
+
+    image - original image
+    char - border of character (rectangle)
+    '''
+
     out_size = 28
-    border_min = 1
+    border_min = 1 # empty pixels along edges
     char_im = get_image_segment(image, char)
     mn = np.shape(char_im)
     m = mn[0]
     n = mn[1]
+
     if m < out_size-border_min and n < out_size-border_min:
       upsample = int(np.floor(min((out_size-border_min)/m, (out_size-border_min)/n)))
       char_im = upsample_image(char_im, upsample)
     elif m > out_size-border_min or n > out_size-border_min:
       downsample = int(np.ceil(max(m/(out_size-border_min), n/(out_size-border_min))))
       char_im = downsample_image(char_im, downsample)
+    
     mn = np.shape(char_im)
     m = mn[0]
     n = mn[1]
+    
     top = int(np.ceil((out_size-m)/2))
     bottom = int(np.floor((out_size-m)/2))
     left = int(np.ceil((out_size-n)/2))
@@ -340,36 +349,62 @@ def char_seg_2_28(image, char):
     new_im = 255*np.ones((out_size,out_size))
 
     new_im[top:(out_size-bottom), left:(out_size-right)] = char_im
+    
     return new_im
 
 
 def get_image_segment(image, rect):
+   '''
+   rectanglue border to image
+
+   image - original image
+   rect - border of segment
+   '''
    return image[rect[0]:rect[1],rect[2]:rect[3]]
 
 def plot_segments(binary_image, orig_image):
-  #plot segmented imaged for visualization
-  fig2 = plt.figure()
+  '''
+  Segment characters
+  '''
 
+  # character segment plot
+  fig2 = plt.figure()
   ax4 = plt.subplot(1,2,2)
   imgplot = plt.imshow(binary_image, cmap='grey')
   plt.title("characters")
-  patch = []
-  rectangles = line_segment(binary_image)
 
 
-  #rectangles = rectangles[3]
-  for rect in rectangles:
-    #fig3 = plt.figure()
-    #img = plt.imshow(get_image_segment(binary_image, rect), cmap='grey')
-    chars, spaces = char_segment(rect, binary_image)
-    char_patches = []
-    for char in chars:
+  line_patches = [] # for overlaying line detection
+  text_detected = [] # string of text found
+  
+  line_borders = line_segment(binary_image) # border of all inie
+ 
+  for line_rect in line_borders:
+    char_borders, spaces = char_segment(line_rect, binary_image)
+    
+    char_patches = [] # for plotting character segments
+    space_i = 0 # for iterating through spaces in line
+    
+    char_prev = char_borders[0][2]# end of previos character
+    
+    for char in char_borders:
+
+      # add in spaces where applicable
+      if space_i != -1 and spaces and (spaces[space_i] <= char[2] and spaces[space_i] > char_prev):
+
+        text_detected.append(' ')
+        space_i += 1
+        if space_i >= len(spaces):
+          space_i = -1
+      
+      char_prev = char[3] # update prevous character end
       char_patches.append(Rectangle([char[2], char[0]], char[3]-char[2], char[1]-char[0], fill=False))
 
-      char_im = char_seg_2_28(orig_image, char)
+      char_im = char_seg_2_28(orig_image, char) # get image to input to model
       prediction = model.predict(char_im.reshape(-1, 28,28,1)/255.0)
-      c = np.argmax(prediction)
+      c = np.argmax(prediction) # label assigned to detected character
 
+      # conver to ascii value
       if (c>=0 and c<=9): ## numbers 0-9"
         new_c = c+48
 
@@ -379,52 +414,57 @@ def plot_segments(binary_image, orig_image):
       elif(c>=36 and c<=61): # lowercase letters
         new_c = c-36+97
 
-
+      # uncomment if you want to see character image
+      '''
       img = plt.imshow(char_im, cmap='grey')
       plt.title(f'The result is likely: {chr(new_c)}')
-      plt.show()
+      plt.show()'
+      '''
 
+      # add detected character
+      text_detected.append(chr(new_c))
 
-  
-    collect1 = PatchCollection(char_patches, facecolor='none', ec='red', linewidth=1)
+    # end of line
+    text_detected.append('\n')
+
+    char_collection = PatchCollection(char_patches, facecolor='none', ec='red', linewidth=1) # for plotting space locations
+    
+    # add character ans space collections to plot
     for space in spaces:
-      ax4.add_line(Line2D([space, space], [rect[0], rect[1]], color='blue'))
-    ax4.add_collection(collect1)
-    patch.append(Rectangle([rect[2], rect[0]], rect[3]-rect[2], rect[1]-rect[0], fill=False))
-
+      ax4.add_line(Line2D([space, space], [line_rect[0], line_rect[1]], color='blue'))
+    ax4.add_collection(char_collection)
+    
+    line_patches.append(Rectangle([line_rect[2], line_rect[0]], line_rect[3]-line_rect[2], line_rect[1]-line_rect[0], fill=False))
+  
+  # print output text
+  print(''.join(text_detected))
+  # tode dump this to .txt file
+  
+  # plot image with line segments
   ax3 = plt.subplot(1,2,1)
   imgplot = plt.imshow(binary_image, cmap='grey')
   plt.title("lines")
-  collect = PatchCollection(patch, facecolor='none', ec='red', linewidth=1)
-  ax3.add_collection(collect)
+  line_collection = PatchCollection(line_patches, facecolor='none', ec='red', linewidth=1)
+  ax3.add_collection(line_collection)
   plt.show()
 
-from pdf2image import convert_from_path
 
-# Store Pdf with convert_from_path function
-#images = convert_from_path(image_path)
-images = [1]
+# main code
+image=Image.open(image_path)# input image location
+image=np.asarray(image)
 
-for i in range(len(images)):
-      # Save pages as images in the pdf
-    #images[i].save('page'+ str(i) +'.jpg', 'JPEG')
-  
-    image=Image.open(image_path)# input image location
-    image=np.asarray(image)
+# plot processed images
+fig = plt.figure()
+ax1 = plt.subplot(1,2,1)
+imgplot = plt.imshow(image)
+plt.title("Original")
 
-    # plot processed images
-    fig = plt.figure()
-    ax1 = plt.subplot(2,2,1)
-    imgplot = plt.imshow(image)
-    plt.title("Original")
+gray_image=grayscale(image) # rgb to grayscale conversion
 
-    gray_image=grayscale(image) # rgb to grayscale conversion
+ax1 = plt.subplot(1,2,2)
+imgplot = plt.imshow(gray_image, cmap='grey')
+plt.title("Greyscale")
 
-    ax1 = plt.subplot(2,2,2)
-    imgplot = plt.imshow(gray_image, cmap='grey')
-    plt.title("Greyscale")
+binary_image = binary(gray_image)
 
-    binary_image = binary(gray_image)
-    # 
-
-    plot_segments(binary_image, gray_image)
+plot_segments(binary_image, gray_image)
